3eba6d1ce40e54f2c54fd09f5cedf015d09fb39b
chenzhenqi <czq19960801@icloud.com>
2020-01-06 11:00:39 +0800

    修改了线程的bug，获取昵称

--- Server.py
+++ Server.py
@@ -3,6 +3,7 @@ import socket
 import time
 
 class Server:
+    Clients = []
     def __init__(self,host,port):
         self.host = host
         self.port = port
@@ -10,15 +11,35 @@ class Server:
         self.network.bind((self.host,self.port))
         self.network.listen(20)
 
+        print(f'server listen at {self.port}')
+
     def start(self):
         while True:
             client_sock,client_addr = self.network.accept()
             print(f'client{client_addr}connected')
+            client_sock.send('HLO'.encode())
             time.sleep(0.1)
-            threading.Thread(target=self.wait_for_user_nickname,args=[client_sock])
+            client_thread = threading.Thread(target=self.wait_for_user_nickname,args=[client_sock])
+            client_thread.start()
 
     def wait_for_user_nickname(self,client_sock):
-        pass
+        new_user_id = client_sock.recv(1024).decode('utf-8')
+        print(new_user_id)
+        client = Client(client_sock,new_user_id)
+        Server.Clients.append(client)
+        client.start()
+
+
+class Client:
+    def __init__(self,sock,clientID):
+        self.connection = sock
+        self.clientID = clientID
+        self._run = True
+
+    def start(self):
+        while self._run:
+            time.sleep(0.1)
+            pass
 
 if __name__ == '__main__':
     server = Server('0.0.0.0',6000)
--- client.py
+++ client.py
@@ -1,17 +1,32 @@
 import socket
+import time
+
 from UserDialog import UserDialog
 
 
 class Connection:
     def __init__(self):
         UserDialog.getUserInputIp()
-        self.host=UserDialog._Ip
-        self.port =UserDialog._port
+        self.host = UserDialog._Ip
+        self.port = UserDialog._port
         print(self.host, self.port)
 
         self.sock = socket.socket()
-        self.sock.connect((self.host,self.port))
+        self.sock.connect((self.host, self.port))
+        data = self.sock.recv(3).decode()
+        print(data)
+
+        UserDialog.getUserNickName()
+        self.nickname = UserDialog._nickname
+
+        self.sock.sendall((self.nickname.encode('utf-8')))
+
+    def start(self):
+        while True:
+            time.sleep(0.1)
+            pass
 
 if __name__ == '__main__':
     con = Connection()
+    con.start()
     print('start')
